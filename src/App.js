c7984f63ffc1f2c724db24e2affd808fe309a8e8
Simon <bardrabbit@gmail.com>
2020-10-31 09:07:43 -0400

    get shades of colors

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 import Palette from './Palette';
 import seedColors from './seedColors';
-
-const App = () => {
-  return (
-    <div>
-      <Palette {...seedColors[6]} />
-    </div>
-  );
-};
+import { generatePalettes } from './colorHelper';
+class App extends Component {
+  render() {
+    console.log(generatePalettes(seedColors[4]));
+    return (
+      <div>
+        <Palette {...seedColors[6]} />
+      </div>
+    );
+  }
+}
 
 export default App;
