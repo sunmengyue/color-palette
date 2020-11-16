af24981be60d1dfa8b48f9634148200b92fcf1a6
Simon <bardrabbit@gmail.com>
2020-11-15 22:52:31 -0500

    make colorbox responsive

--- src/styles/PaletteStyle.js
+++ src/styles/PaletteStyle.js
@@ -1,3 +1,4 @@
+import sizes from './sizes';
 export default {
   Palette: {
     height: '100vh',
@@ -38,6 +39,18 @@ export default {
       border: 'none',
       fontSize: '1rem',
       textDecoration: 'none',
+    },
+    [sizes.down('lg')]: {
+      width: '25%',
+      height: '33.333%',
+    },
+    [sizes.down('md')]: {
+      width: '50%',
+      height: '20%',
+    },
+    [sizes.down('xs')]: {
+      width: '100%',
+      height: '10%',
     },
   },
 };
