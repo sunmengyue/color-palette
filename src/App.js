6f16f05887fd897e04cf4333c86bbd1cc6d14416
Simon <bardrabbit@gmail.com>
2020-11-16 22:01:01 -0500

    add basic route transitions

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import SingleColorPalette from './SingleColorPalette';
 import seedColors from './seedColors';
 import PaletteList from './PaletteList';
 import { generatePalettes } from './colorHelper';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 class App extends Component {
   constructor(props) {
     super(props);
@@ -44,53 +45,69 @@ class App extends Component {
 
   render() {
     return (
-      <Switch>
-        <Route
-          exact
-          path='/palette/newForm'
-          render={(routeProps) => (
-            <NewPaletteForm
-              palettes={this.state.palettes}
-              savePalette={this.savePalette}
-              {...routeProps}
-            />
-          )}
-        />
-        <Route
-          exact
-          path='/palette/:paletteId/:colorId'
-          render={(routeProps) => (
-            <SingleColorPalette
-              colorId={routeProps.match.params.colorId}
-              palette={generatePalettes(
-                this.findPalette(routeProps.match.params.paletteId)
-              )}
-            />
-          )}
-        />
-        <Route
-          exact
-          path='/'
-          render={(routeProps) => (
-            <PaletteList
-              palettes={this.state.palettes}
-              {...routeProps}
-              deletePalette={this.deletePalette}
-            />
-          )}
-        />
-        <Route
-          exact
-          path='/palette/:id'
-          render={(routeProps) => (
-            <Palette
-              palette={generatePalettes(
-                this.findPalette(routeProps.match.params.id)
-              )}
-            />
-          )}
-        />
-      </Switch>
+      <Route
+        render={({ location }) => (
+          <TransitionGroup>
+            <CSSTransition key={location.key} classNames='fade' timeout={500}>
+              <Switch location={location}>
+                <Route
+                  exact
+                  path='/palette/newForm'
+                  render={(routeProps) => (
+                    <div className='page'>
+                      <NewPaletteForm
+                        palettes={this.state.palettes}
+                        savePalette={this.savePalette}
+                        {...routeProps}
+                      />
+                    </div>
+                  )}
+                />
+                <Route
+                  exact
+                  path='/palette/:paletteId/:colorId'
+                  render={(routeProps) => (
+                    <div className='page'>
+                      <SingleColorPalette
+                        colorId={routeProps.match.params.colorId}
+                        palette={generatePalettes(
+                          this.findPalette(routeProps.match.params.paletteId)
+                        )}
+                      />
+                    </div>
+                  )}
+                />
+                <Route
+                  exact
+                  path='/'
+                  render={(routeProps) => (
+                    <div className='page'>
+                      <PaletteList
+                        palettes={this.state.palettes}
+                        {...routeProps}
+                        deletePalette={this.deletePalette}
+                      />
+                    </div>
+                  )}
+                />
+                <Route
+                  exact
+                  path='/palette/:id'
+                  render={(routeProps) => (
+                    <div className='page'>
+                      <Palette
+                        palette={generatePalettes(
+                          this.findPalette(routeProps.match.params.id)
+                        )}
+                      />
+                    </div>
+                  )}
+                />
+              </Switch>
+            </CSSTransition>
+          </TransitionGroup>
+        )}
+      />
     );
   }
 }
